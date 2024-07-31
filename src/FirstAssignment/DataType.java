50a5927ede89cebea486e8036cea4505a7bb0644
Yash Somani <57716157+yhsomani@users.noreply.github.com>
2024-07-31 19:36:24 +0530

    Create DataType.java

---
+++ src/FirstAssignment/DataType.java
@@ -0,0 +1,39 @@
+public class DataType
+{
+public static void main(String[] args) {
+    //boolean
+boolean bt = true;
+        System.out.println(bt);
+        
+        //byte
+        byte n;
+        n = 127;
+        System.out.println(n); // prints 127
+        
+        //char
+        char a = 'A';
+        char b = 66;
+        System.out.println(a); // prints A
+        System.out.println(b); // Displays B
+
+        //short
+        short m= 1234;
+        System.out.println(m);
+        
+        //int
+        int num1=56782;
+        System.out.println(num1);
+        
+        //long
+        long num2 = 15000000000L;
+        System.out.println(num2);
+        
+        //float
+        float num3 =3.14f;
+        System.out.println(num3);
+
+        //double
+        double num4 = 69.784d;
+        System.out.println(num4);
+  }
+}
